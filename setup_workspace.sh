31bb3d4db19dad101ea0fdfdee23ac831992ca30
ShadowSec_mohit <suresh19309936637693@gmail.com>
2026-07-05 12:44:17 +0530

    Create setup_workspace.sh

---
+++ setup_workspace.sh
@@ -0,0 +1,15 @@
+scan 
+reports
+exploits
+Folders ready Agent!
+
+2. 📄 setup_workspace.sh (Auto Folder Creator)
+Kyun banayi: Cyber security ka workspace ek jhatke mein setup karne ke liye (Automation).
+
+Isme use hui commands:
+
+#!/bin/bash — Shebang line.
+
+mkdir — Ek sath teen alag-alag naye folders (scans, exploits, reports) banane ke liye.
+
+echo — Kaam poora hone par confirmation message dikhane ke liye.
