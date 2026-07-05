23616348254a198e5840319e458653eab7c0e13b
ShadowSec_mohit <suresh19309936637693@gmail.com>
2026-07-05 12:41:27 +0530

    Create welcome_agent.sh

---
+++ welcome_agent.sh
@@ -0,0 +1,17 @@
+Hello Agent Mohit
+Sun Jul  5 03:10:10 AM EDT 2026
+/home/kali
+
+
+1. 📄 welcome_agent.sh (The Welcome Hacker Script)
+Kyun banayi: Yeh script system ka swagat karti hai aur current working context batati hai.
+
+Isme use hui commands:
+
+#!/bin/bash — Shebang line (Script ko batane ke liye ki ise Bash se chalana hai).
+
+echo — Screen par text print karne ke liye.
+
+date — System ka current time aur date dikhane ke liye.
+
+pwd — Aap abhi kis folder mein ho (Present Working Directory) woh check karne ke liye.
